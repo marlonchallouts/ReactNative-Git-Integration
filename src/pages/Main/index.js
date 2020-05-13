177f9a466451898cafe2e298801e8c07a285a7d0
Marlon Challouts <marlon.challouts@gmail.com>
2020-05-13 14:13:31 -0300

    Estilizando formulário

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
-
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 const Main = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 };
 
 export default Main;
