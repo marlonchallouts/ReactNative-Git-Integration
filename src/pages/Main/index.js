1656f54afc12f21d37ebf49c10c33d9521200512
Marlon Challouts <marlon.challouts@gmail.com>
2020-05-13 13:24:19 -0300

    Instalando e configurando o React Navigation

---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Main = () => {
+  return <View />;
+};
+
+export default Main;
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
