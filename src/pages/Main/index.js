b42bcba8766bb49d5b298a7db151a65a977a6481
Marlon Challouts <marlon.challouts@gmail.com>
2020-05-13 13:39:37 -0300

    Instalando e configurando os Styled Components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const Main = () => {
-  return <View />;
+  return <Container />;
 };
 
 export default Main;
