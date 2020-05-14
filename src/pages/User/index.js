752f5e3dae1f8e429092353bbb26dfe01b0b7838
Marlon Challouts <marlon.challouts@gmail.com>
2020-05-14 18:14:48 -0300

    Configurando navegação entre telas Configurando PropTypes Buscando dados do usuário pela api

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
+import Proptypes from 'prop-types';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-const User = () => {
-  return <View />;
-};
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
 
-export default User;
+  static propTypes = {
+    navigation: Proptypes.shape({
+      getParam: Proptypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+    return <View />;
+  }
+}
